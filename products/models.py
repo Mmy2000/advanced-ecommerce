163a59d9d27512e978448fa58e519666804b7e6d
Mmy2000 <yahianada005@gmail.com>
2024-02-11 22:22:05 +0200

    creating product app and adding thier models part 6

--- products/models.py
+++ products/models.py
@@ -27,6 +27,11 @@ class Product(models.Model):
     class Meta:
         ordering = ["name"]
 
+    def save(self,*args, **kwargs):
+        if not self.slug:
+            self.slug=slugify(self.name)
+        super(Product,self).save(*args,**kwargs)
+
     def __str__(self):
         return self.name
 
