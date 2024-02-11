e061e37ad54f58ed51281fa55d7ebaa2211f138f
Mmy2000 <yahianada005@gmail.com>
2024-02-11 22:18:45 +0200

    creating product app and adding thier models part 4

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from .models import Product , Category , Subcategory , Brand
 # Register your models here.
-admin.site.register(Product)
+
+class SubCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name','category')
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name' , 'price'  , 'subcategory' , 'PRDBrand' , 'stock' , 'created_at' , 'is_available')
+
+admin.site.register(Product , ProductAdmin)
 admin.site.register(Category)
-admin.site.register(Subcategory)
+admin.site.register(Subcategory , SubCategoryAdmin)
 admin.site.register(Brand)
