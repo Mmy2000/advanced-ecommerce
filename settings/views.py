f16289f08e290638f06cdb9bbb95ba4b588029a5
Mmy2000 <yahianada005@gmail.com>
2024-02-14 16:30:05 +0200

    creating product app and adding thier models part 42

--- settings/views.py
+++ settings/views.py
@@ -4,8 +4,10 @@ from products.models import Product
 # Create your views here.
 def home(request):
     trandy_paroduct = Product.objects.all().order_by('-views')
+    just_arrived = Product.objects.all().order_by('-created_at')
 
     context = {
-        'trandy_product':trandy_paroduct
+        'trandy_product':trandy_paroduct,
+        'just_arrived':just_arrived
     }
     return render(request , 'home.html' , context)
