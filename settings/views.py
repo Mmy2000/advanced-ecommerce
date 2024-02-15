c484d9f394a097c7518b781256efaf75e4a4c370
Mmy2000 <yahianada005@gmail.com>
2024-02-16 01:02:57 +0200

    creating product app and adding thier models part 51

--- settings/views.py
+++ settings/views.py
@@ -5,7 +5,7 @@ from django.db.models import Count
 def home(request):
     trandy_paroduct = Product.objects.all().order_by('-views')
     just_arrived = Product.objects.all().order_by('-created_at')
-    category = Subcategory.objects.all().annotate(category_count=Count("product_subcategory"))
+    category = Subcategory.objects.all().annotate(category_count=Count("product_subcategory"))[:3]
 
     context = {
         'trandy_product':trandy_paroduct,
