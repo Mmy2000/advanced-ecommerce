d48a663eb92835530282299a76e2009d8bab583a
Mmy2000 <yahianada005@gmail.com>
2024-02-16 16:29:35 +0200

    creating contact page part 8

--- settings/models.py
+++ settings/models.py
@@ -9,3 +9,18 @@ class Contact(models.Model):
 
     def __str__(self):
         return self.email
+    
+class Settings(models.Model):
+    site_name = models.CharField( max_length=50)
+    logo = models.ImageField( upload_to='setting/')
+    phone = models.CharField( max_length=30)
+    email = models.EmailField( max_length=254)
+    description = models.TextField(max_length=1000)
+    fb_link = models.URLField( max_length=200)
+    twitter_link = models.URLField( max_length=200)
+    instagram_link = models.URLField( max_length=200)
+    address = models.CharField( max_length=50)
+    get_in_touch = models.TextField(max_length=1000)
+
+    def __str__(self):
+        return self.site_name
