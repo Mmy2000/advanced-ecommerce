a5ac0d8e6c85b2a56d55ccf65226e4057422380b
Mmy2000 <yahianada005@gmail.com>
2024-02-18 04:59:38 +0200

    create cart and cart item functionallity part 8

--- carts/urls.py
+++ carts/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     path('', views.cart , name='cart'),
     path('add_cart/<int:product_id>/', views.add_cart , name='add_cart'),
+    path('decrement_cart/<int:product_id>/', views.decrement_cart , name='decrement_cart'),
 ]
