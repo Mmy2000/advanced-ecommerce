811a9a15d274c3a663f168789fc7528ab87f5a1a
Mmy2000 <yahianada005@gmail.com>
2024-02-18 04:29:21 +0200

    create cart and cart item functionallity part 3

--- carts/views.py
+++ carts/views.py
@@ -39,7 +39,7 @@ def cart(request , total = 0 , quantity = 0 , cart_items = None):
         cart = Cart.objects.get(cart_id=_cart_id(request))
         cart_items = CartItem.objects.filter(cart=cart , is_active = True)
         for cart_item in cart_items:
-            total += (cart_item.price * cart_item.quantity)
+            total += (cart_item.product.price * cart_item.quantity)
             quantity += cart_item.quantity
     except Cart.DoesNotExist:
         pass
