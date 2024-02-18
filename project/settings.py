e9595576ab00bde4a4b7f52aa48b2d7b236a3068
Mmy2000 <yahianada005@gmail.com>
2024-02-18 05:15:48 +0200

    create cart and cart item functionallity part 10

--- project/settings.py
+++ project/settings.py
@@ -72,6 +72,7 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'products.product_context.category_nav',
                 'settings.settings_context.my_settings',
+                'carts.cart_context.cart_count',
             ],
         },
     },
