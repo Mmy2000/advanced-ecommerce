4b3d50ef4349f45e04e9e7f705c3d28dc8d6d00d
Mmy2000 <yahianada005@gmail.com>
2024-02-18 03:30:41 +0200

    starting with cart app

--- project/urls.py
+++ project/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('' , include('settings.urls') ),
     path('products/' , include('products.urls') ),
+    path('cart/' , include('carts.urls') ),
 ]
 urlpatterns +=  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
