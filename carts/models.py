2cb9d46f1df2c90386fa221bfa9e960bd8b1f715
Mmy2000 <yahianada005@gmail.com>
2024-02-18 03:43:16 +0200

    create cart and cart item models

--- carts/models.py
+++ carts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils import timezone
 
+# Create your models here.
+class Cart(models.Model):
+    cart_id = models.CharField( max_length=50 , blank=True)
+    date_added = models.DateField(default=timezone.now())
+
+
+    def __str__(self):
+        return self.cart_id
+    
+class CartItem(models.Model):
+    product = models.ForeignKey("products.Product", on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE , null=True)
+    quantity = models.IntegerField()
+    is_active = models.BooleanField(default=True)
+
+
+    def __str__(self):
+        return str(self.product)
 # Create your models here.
