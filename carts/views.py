4ef26be50c5d45ecb2ec45bb07e6883bd7ec50ec
Mmy2000 <yahianada005@gmail.com>
2024-02-19 06:12:22 +0200

    create cart and cart item functionallity part 24

--- carts/views.py
+++ carts/views.py
@@ -13,11 +13,11 @@ def _cart_id(request):
 
 
 def add_cart(request,product_id):
-    color = request.GET['color']
-    size = request.GET['size']
-    quantity = request.GET['quantity']
-    return HttpResponse(color  +' '+ size +' '+quantity)
-    exit()
+    if request.method == "POST":
+        color = request.POST['color']
+        size = request.POST['size']
+        quantity = request.POST['quantity']
+        print(color , size , quantity)
     product = Product.objects.get(id=product_id)
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request))
