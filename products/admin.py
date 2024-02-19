bfc52e8bd66be06e2175ad02c89fbf9560f28cfa
Mmy2000 <yahianada005@gmail.com>
2024-02-19 04:49:41 +0200

    create cart and cart item functionallity part 19

--- products/admin.py
+++ products/admin.py
@@ -13,9 +13,14 @@ class ProductAdmin(admin.ModelAdmin):
     
     get_category.short_description = 'Category'
 
+class VariationAdmin(admin.ModelAdmin):
+    list_display = ('product' , 'variation_category' , 'variation_value' , 'created_at' , 'is_active' )
+    list_editable = ('is_active',)
+    list_filter = ('product' , 'variation_category' , 'variation_value')
+
 admin.site.register(Product , ProductAdmin)
 admin.site.register(Category)
 admin.site.register(Subcategory , SubCategoryAdmin)
 admin.site.register(Brand)
 admin.site.register(ProductImages)
-admin.site.register(Variation)
+admin.site.register(Variation,VariationAdmin)
