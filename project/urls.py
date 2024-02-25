51285b302e6f483511a8a0aa9853d345ce951f2c
Mmy2000 <yahianada005@gmail.com>
2024-02-25 05:48:04 +0200

    django authintication

--- project/urls.py
+++ project/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('' , include('settings.urls') ),
     path('products/' , include('products.urls') ),
     path('cart/' , include('carts.urls') ),
+    path('accounts/' , include('accounts.urls') ),
 ]
 urlpatterns +=  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
