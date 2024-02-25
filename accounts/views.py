1955875c97a1d09e0b754ebe4abbe96d9fa54c2d
Mmy2000 <yahianada005@gmail.com>
2024-02-25 06:04:43 +0200

    django authintication

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from .forms import RegistrationForm
+from django.contrib.auth import authenticate , login 
 
 # Create your views here.
+
 def register(request):
-    return render(request,'accounts/register.html')
+    form=RegistrationForm()
+    context = {
+        'form':form
+    }
+    return render(request,'accounts/register.html' , context)
