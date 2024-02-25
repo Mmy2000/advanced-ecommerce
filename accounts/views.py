0c3700de27c5ea20dc748f1d5ef094b08be7c607
Mmy2000 <yahianada005@gmail.com>
2024-02-25 06:32:14 +0200

    django authintication

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,42 @@
 from django.shortcuts import render , redirect
 from .forms import RegistrationForm
 from django.contrib.auth import authenticate , login 
+from .models import User
 
 # Create your views here.
 
 def register(request):
-    form=RegistrationForm()
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            phone_number = form.cleaned_data['phone_number']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            username = email.split("@")[0]
+            user = User.objects.create_user(first_name=first_name, last_name=last_name, email=email, username=username, password=password)
+            user.phone_number = phone_number
+            user.save()
+
+            # # USER ACTIVATION
+            # current_site = get_current_site(request)
+            # mail_subject = 'Please activate your account'
+            # message = render_to_string('accounts/account_verification_email.html', {
+            #     'user': user,
+            #     'domain': current_site,
+            #     'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+            #     'token': default_token_generator.make_token(user),
+            # })
+            # to_email = email
+            # send_email = EmailMessage(mail_subject, message, to=[to_email])
+            # send_email.send()
+            # #messages.success(request, 'Thank you for registering with us. We have sent you a verification email to your email address , Please verify it.')
+            # return redirect('/accounts/login/?command=verification&email='+email)
+        
+
+    else:
+        form = RegistrationForm()
     context = {
         'form':form
     }
