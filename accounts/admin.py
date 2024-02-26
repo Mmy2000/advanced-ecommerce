18910fce5f1d74a1ab4192baa0f07dff2df174a9
Mmy2000 <yahianada005@gmail.com>
2024-02-26 05:12:59 +0200

    django authintication

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User , Profile
 
 # Register your models here.
 
@@ -15,3 +15,4 @@ class AccountAdmin(UserAdmin):
     fieldsets = ()
 
 admin.site.register(User, AccountAdmin)
+admin.site.register(Profile)
--- accounts/models.py
+++ accounts/models.py
@@ -74,3 +74,32 @@ class User(AbstractBaseUser):
 
     def has_module_perms(self, add_label):
         return True
+    
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    address = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='users_images/')
+    about = models.TextField(max_length=4000 , blank=True , null=True)
+    country = models.CharField(max_length=50 ,blank=True, null=True)
+    company = models.CharField(max_length=100 ,blank=True, null=True)
+    address_line_1 = models.CharField( max_length=50 , blank=True)
+    address_line_2 = models.CharField( max_length=50 , blank=True)
+    headline = models.CharField(max_length=50 , blank=True, null=True)
+    city = models.CharField(max_length=50 ,blank=True, null=True)
+
+
+    def full_name(self):
+        return f'{self.user.first_name} {self.user.last_name}'
+    
+    def full_address(self):
+        return f"{self.country} | {self.city} | {self.address_line_1} {self.address_line_2}"
+
+
+
+    def __str__(self):
+        return str(self.user)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
