573dff1282dc1feb07a82ba8f4049eafb3077829
Mmy2000 <yahianada005@gmail.com>
2024-02-27 09:44:48 +0200

    django authintication

--- accounts/views.py
+++ accounts/views.py
@@ -131,7 +131,24 @@ def resetpassword_validate(request, uidb64, token):
     else:
         messages.error(request, 'This link has been expired!')
         return redirect('login')
-    
+
+def resetPassword(request):
+    if request.method == 'POST':
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        if password == confirm_password:
+            uid = request.session.get('uid')
+            user = User.objects.get(pk=uid)
+            user.set_password(password)
+            user.save()
+            messages.success(request, 'Password reset successful')
+            return redirect('login')
+        else:
+            messages.error(request, 'Password do not match!')
+            return redirect('resetPassword')
+    else:
+        return render(request, 'accounts/resetPassword.html')
 
 def profile(request):
     profile = Profile.objects.get(user=request.user)
