e2d544ed1f3ce1ae87efb053fb873c844914a063
Mmy2000 <yahianada005@gmail.com>
2024-03-02 21:28:11 +0200

    cart user

--- carts/views.py
+++ carts/views.py
@@ -117,12 +117,13 @@ def add_cart(request,product_id):
         return redirect('cart')
 
 def decrement_cart(request , product_id,cart_item_id):
-    cart = Cart.objects.get(cart_id = _cart_id(request))
+    
     product = get_object_or_404(Product , id=product_id)
     try:
         if request.user.is_authenticated:
             cart_item = CartItem.objects.get(product=product , user = request.user , id=cart_item_id)
         else:
+            cart = Cart.objects.get(cart_id = _cart_id(request))
             cart_item = CartItem.objects.get(product=product , cart = cart , id=cart_item_id)
         if cart_item.quantity > 1:
             cart_item.quantity -= 1
