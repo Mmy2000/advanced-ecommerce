758216894e61a6728618ed4f9c5ffd19ce8c1563
Mmy2000 <yahianada005@gmail.com>
2024-03-03 21:14:54 +0200

    newsletter

--- settings/urls.py
+++ settings/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from . views import home , contact , newsletters
+from . views import home , contact , newsletters 
+from . settings_context import newsletter_footer
 
 urlpatterns = [
     path('', home ,name = 'home'),
     path('contact/', contact ,name = 'contact'),
     path('newsletters/', newsletters ,name = 'newsletters'),
+    path('newsletter_footer/', newsletter_footer ,name = 'newsletter_footer'),
 ]
