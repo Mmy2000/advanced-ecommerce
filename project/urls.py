24c5b9bc4ce03dc26d3f61197fe21b71008a3a0f
Mmy2000 <yahianada005@gmail.com>
2024-03-04 20:27:19 +0200

    starting the view for order app

--- project/urls.py
+++ project/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('' , include('settings.urls') ),
     path('products/' , include('products.urls') ),
     path('cart/' , include('carts.urls') ),
+    path('orders/' , include('orders.urls') ),
     path('accounts/' , include('accounts.urls') ),
 ]
 urlpatterns +=  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
