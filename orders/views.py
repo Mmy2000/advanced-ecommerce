f3792770faeccb4e318d561e103ec38029d32e03
Mmy2000 <yahianada005@gmail.com>
2024-03-04 20:35:32 +0200

    starting the view for order app

--- orders/views.py
+++ orders/views.py
@@ -28,4 +28,31 @@ def place_order(request):
     grand_total = total + tax
     if request.method == 'POST':
         form = OrderForm(request.POST)
-        
+        if form.is_valid():
+            # Store all the billing information inside Order table
+            data = Order()
+            data.user = current_user
+            data.first_name = form.cleaned_data['first_name']
+            data.last_name = form.cleaned_data['last_name']
+            data.phone = form.cleaned_data['phone']
+            data.zip_code = form.cleaned_data['zip_code']
+            data.email = form.cleaned_data['email']
+            data.address_line_1 = form.cleaned_data['address_line_1']
+            data.address_line_2 = form.cleaned_data['address_line_2']
+            data.country = form.cleaned_data['country']
+            data.state = form.cleaned_data['state']
+            data.city = form.cleaned_data['city']
+            data.order_note = form.cleaned_data['order_note']
+            data.order_total = grand_total
+            data.tax = tax
+            data.ip = request.META.get('REMOTE_ADDR')
+            data.save()
+            # Generate order number
+            yr = int(datetime.date.today().strftime('%Y'))
+            dt = int(datetime.date.today().strftime('%d'))
+            mt = int(datetime.date.today().strftime('%m'))
+            d = datetime.date(yr,mt,dt)
+            current_date = d.strftime("%Y%m%d") #20210305
+            order_number = current_date + str(data.id)
+            data.order_number = order_number
+            data.save()
