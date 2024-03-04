93542dc2891fd88659c73763513a118a5bf4ffeb
Mmy2000 <yahianada005@gmail.com>
2024-03-04 20:33:26 +0200

    starting the view for order app

--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,31 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from carts.models import CartItem
+from .forms import OrderForm
+from .models import Order, Payment, OrderProduct
+from products.models import Product
+import datetime
+import json
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
+from django.http import HttpResponse, JsonResponse
+from django.utils import timezone
+from django.urls import reverse
+from django.contrib import messages
 # Create your views here.
 def place_order(request):
-    pass
+    current_user = request.user
+    # If the cart count is less than or equal to 0, then redirect back to shop
+    cart_items = CartItem.objects.filter(user=current_user)
+    cart_count = cart_items.count()
+    if cart_count <= 0:
+        return redirect('product_list')
+    grand_total = 0
+    tax = 0
+    for cart_item in cart_items:
+        total+=(cart_item.product.price * cart_item.quantity)
+        quantity+=cart_item.quantity
+    tax = (2 * total)/100
+    grand_total = total + tax
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        
