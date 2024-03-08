9534e62fa8d11f20ef2e2edfe6a6618114d08c6f
Mmy2000 <yahianada005@gmail.com>
2024-03-08 20:23:19 +0200

    starting the view for order app

--- orders/views.py
+++ orders/views.py
@@ -70,3 +70,6 @@ def place_order(request,total=0, quantity=0):
              return redirect('checkout')
     else:
         return redirect('checkout')
+
+def payments(request):
+    return render(request , 'payment.html')
