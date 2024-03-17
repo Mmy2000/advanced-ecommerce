52054658523de96b45c15d582aba3c5b7022a972
Mmy2000 <yahianada005@gmail.com>
2024-03-17 02:50:57 +0200

    updating

--- accounts/views.py
+++ accounts/views.py
@@ -209,7 +209,7 @@ def change_password(request):
                 user.set_password(new_password)
                 user.save()
                 messages.success(request,'Password Updated Successfully.')
-                return redirect('profile')
+                return redirect('change_password')
             else:
                 messages.error(request,'Please enter a valid current password.')
                 return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
