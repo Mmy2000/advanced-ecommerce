1d060551dfe471956a77adee126a01719716802f
Mmy2000 <yahianada005@gmail.com>
2024-05-17 14:09:22 +0300

    updating

--- products/models.py
+++ products/models.py
@@ -100,7 +100,7 @@ class Category(models.Model):
         return self.name
 
 class Subcategory(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE , blank=True , null=True)
     name = models.CharField(max_length=100)
     image = models.ImageField(null=True,blank=True,upload_to='category-image/')
 
