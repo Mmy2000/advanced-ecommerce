37b622aa39a0bea54096648ed9aa4ff782f2dc50
Mmy2000 <yahianada005@gmail.com>
2024-05-27 17:49:38 +0300

    craeting api for accounts app

--- accounts/api_view.py
+++ accounts/api_view.py
@@ -1,6 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
 from .models import User
 from .serializer import UserSerializer
 
@@ -9,6 +10,11 @@ def register(request):
     if request.method == 'POST':
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
-            return Response({'message': 'User registered successfully'}, status=status.HTTP_201_CREATED)
+            user = serializer.save()
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                'message': 'User registered successfully',
+                'refresh token': str(refresh),
+                'access token': str(refresh.access_token),
+            }, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
