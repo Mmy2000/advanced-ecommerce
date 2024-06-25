73fb39cbd13674d4289dade2bfefc45fb7e52c66
Mmy2000 <yahianada005@gmail.com>
2024-06-25 22:46:43 +0300

    updating

--- settings/admin.py
+++ settings/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Contact , Settings , NewsLitter  , About , FAQ
+from .models import Contact , Settings , NewsLitter  , About , FAQ , Images
 # Register your models here.
 admin.site.register(Contact)
 admin.site.register(Settings)
 admin.site.register(NewsLitter)
 admin.site.register(About)
+admin.site.register(Images)
 admin.site.register(FAQ)
