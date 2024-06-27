3bcb26ed5ef25adeeab227cebb7aaf460157a4e6
Mmy2000 <yahianada005@gmail.com>
2024-06-28 00:13:55 +0300

    updating

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,14 +1,16 @@
 from django import forms
 from .models import  Profile 
 from accounts.models import User
+from django.utils.translation import gettext_lazy as _
+
 
 class RegistrationForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput(attrs={
-        'placeholder': 'Enter Password',
+        'placeholder': _('Enter Password'),
         'class': 'form-control',
     }))
     confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
-        'placeholder': 'Confirm Password'
+        'placeholder': _('Confirm Password')
     }))
 
     class Meta:
@@ -17,10 +19,10 @@ class RegistrationForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(RegistrationForm, self).__init__(*args, **kwargs)
-        self.fields['first_name'].widget.attrs['placeholder'] = 'Enter First Name'
-        self.fields['last_name'].widget.attrs['placeholder'] = 'Enter last Name'
-        self.fields['phone_number'].widget.attrs['placeholder'] = 'Enter Phone Number'
-        self.fields['email'].widget.attrs['placeholder'] = 'Enter Email Address'
+        self.fields['first_name'].widget.attrs['placeholder'] = _('Enter First Name')
+        self.fields['last_name'].widget.attrs['placeholder'] = _('Enter last Name')
+        self.fields['phone_number'].widget.attrs['placeholder'] = _('Enter Phone Number')
+        self.fields['email'].widget.attrs['placeholder'] = _('Enter Email Address')
         
         for field in self.fields:
             self.fields[field].widget.attrs['class'] = 'form-control'
--- products/utils.py
+++ products/utils.py
@@ -1,3 +1,3 @@
 def can_access_rosetta(user):
     # Only allow superusers or users with a specific permission
-    return user.is_superadmin or user.has_perm('products.can_translate')
+    return user.is_superadmin or  user.has_perm('products.can_translate') or user.has_perm('accounts.can_translate')
