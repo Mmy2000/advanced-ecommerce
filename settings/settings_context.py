34061192e9ab4396f86791b19de50fbbbeefe6fc
Mmy2000 <yahianada005@gmail.com>
2024-06-28 08:31:18 +0300

    updating

--- settings/settings_context.py
+++ settings/settings_context.py
@@ -1,10 +1,13 @@
 from .models import Settings , NewsLitter
 from django.http import JsonResponse
 from django.shortcuts import redirect
+from django.utils import translation
+
 
 def my_settings(request):
+    current_language = translation.get_language()
     about_site = Settings.objects.last()
-    return {'about_site':about_site}
+    return {'about_site':about_site,'current_language':current_language}
 
 def newsletter_footer(request):
     email = request.POST.get('email')
