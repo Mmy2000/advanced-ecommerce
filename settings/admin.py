695322ccc95a7637932c5550209835ad669045d4
Mmy2000 <yahianada005@gmail.com>
2024-07-02 12:38:08 +0300

    updating

--- settings/admin.py
+++ settings/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from .models import Contact , Settings , NewsLitter  , About , FAQ , Images
+from parler.admin import TranslatableAdmin
+
 # Register your models here.
 admin.site.register(Contact)
-admin.site.register(Settings)
+admin.site.register(Settings,TranslatableAdmin)
 admin.site.register(NewsLitter)
-admin.site.register(About)
-admin.site.register(Images)
-admin.site.register(FAQ)
+admin.site.register(About,TranslatableAdmin)
+admin.site.register(Images,TranslatableAdmin)
+admin.site.register(FAQ,TranslatableAdmin)
--- settings/models.py
+++ settings/models.py
@@ -5,6 +5,7 @@ from django.utils.text import slugify
 from django.utils.translation import gettext_lazy as _
 from accounts.models import User
 from django.urls import reverse
+from parler.models import TranslatableModel, TranslatedFields
 
 # Create your models here.
 class Contact(models.Model):
@@ -20,17 +21,20 @@ class Contact(models.Model):
     def __str__(self):
         return self.email
     
-class Settings(models.Model):
-    site_name = models.CharField(_("site_name"), max_length=50)
+class Settings(TranslatableModel):
+    translations = TranslatedFields(
+        site_name = models.CharField(_("site_name"), max_length=50),
+        address = models.CharField(_("address"), max_length=50),
+        get_in_touch = models.TextField(_("get_in_touch"),max_length=1000),
+        phone = models.CharField(_("phone"), max_length=30),
+        description = models.TextField(_("description"),max_length=1000)
+    )
+    
     logo = models.ImageField(_("logo"), upload_to='setting/')
-    phone = models.CharField(_("phone"), max_length=30)
     email = models.EmailField(_("email"), max_length=254)
-    description = models.TextField(_("description"),max_length=1000)
     fb_link = models.URLField(_("fb_link"), max_length=200)
     twitter_link = models.URLField(_("twitter_link"), max_length=200)
     instagram_link = models.URLField(_("instagram_link"), max_length=200)
-    address = models.CharField(_("address"), max_length=50)
-    get_in_touch = models.TextField(_("get_in_touch"),max_length=1000)
 
 
     class Meta:
@@ -53,11 +57,14 @@ class NewsLitter(models.Model):
     def __str__(self):
         return self.email
     
-class About(models.Model):
-    what_we_do = models.TextField(_("what_we_do"), max_length=5000)
-    our_mission = models.TextField(_("our_mission"),max_length=5000)
-    our_gaols = models.TextField(_("our_gaols"),max_length=5000)
+class About(TranslatableModel):
+    
     image = models.ImageField(_("image"), upload_to='about/')
+    translations = TranslatedFields(
+        what_we_do = models.TextField(_("what_we_do"), max_length=5000),
+        our_mission = models.TextField(_("our_mission"),max_length=5000),
+        our_gaols = models.TextField(_("our_gaols"),max_length=5000),
+    )
 
     class Meta:
         verbose_name = _("About")
@@ -66,9 +73,12 @@ class About(models.Model):
     def __str__(self):
         return str(self.id)
     
-class FAQ(models.Model):
-    title = models.CharField(_("title"), max_length=150)
-    description = models.TextField(_("description"),max_length=3000)
+class FAQ(TranslatableModel):
+    
+    translations = TranslatedFields(
+        title = models.CharField(_("title"), max_length=150),
+        description = models.TextField(_("description"),max_length=3000)
+    )
 
     class Meta:
         verbose_name = _("FAQ")
@@ -77,10 +87,13 @@ class FAQ(models.Model):
     def __str__(self):
         return self.title
     
-class Images(models.Model):
+class Images(TranslatableModel):
+    translations = TranslatedFields(
+        title = models.CharField(_("title"), max_length=150 , help_text=_('this title is display in slider in home page')),
+        offer = models.CharField(_("offer"), max_length=250 , null=True , blank=True)
+    )
     settings = models.ForeignKey(Settings, related_name='home_image',verbose_name="home_image", on_delete=models.CASCADE)
-    title = models.CharField(_("title"), max_length=150 , help_text='this title is display in slider in home page')
-    offer = models.CharField(_("offer"), max_length=250 , null=True , blank=True)
+    
     image = models.ImageField(_("image"), upload_to='homeImages/' , help_text='this image is display in slider in home page')
 
     class Meta:
